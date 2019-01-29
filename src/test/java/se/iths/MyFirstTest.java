27d3a321046bd7d6d1aff8730b062f1c82d69383
Johan Bäckström <johan.backstrom@claremont.se>
2019-01-29 14:26:00 +0100

    Added some tests

---
+++ src/test/java/se/iths/MyFirstTest.java
@@ -0,0 +1,82 @@
+package se.iths;
+
+import junit.lab.Calculator;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyFirstTest {
+
+    Calculator myCalculator = new Calculator();
+
+    @Test
+    public void addPositiveNumbers() {
+
+        int additionResult = myCalculator.add(1, 2);
+        Assert.assertEquals(3, additionResult);
+    }
+
+    @Test
+    public void addNegativeNumbers() {
+
+        int additionResult = myCalculator.add(-100, -2);
+        Assert.assertEquals(-102, additionResult);
+    }
+
+    @Test
+    public void subtractTwoNumbers() {
+        Assert.assertEquals(
+                50,
+                myCalculator.subtract(100, 50)
+        );
+    }
+
+    @Test
+    public void percentageOfPositiveNumber() {
+        Assert.assertEquals(
+                200,
+                myCalculator.percent(50, 25),
+                0
+        );
+    }
+
+    @Test
+    public void percentageOfNegativeNumber() {
+        Assert.assertEquals(
+                -200,
+                myCalculator.percent(50, -25),
+                0
+        );
+    }
+
+    @Test
+    public void percentageOfZero() {
+
+        boolean isInfinite = Double.isInfinite(
+                myCalculator.percent(50, 0)
+        );
+
+        Assert.assertTrue(
+            isInfinite
+        );
+    }
+
+    @Test
+    public void divisionByZero(){
+
+        try {
+            myCalculator.divide(100, 0);
+        } catch (ArithmeticException e){
+            return;
+        }
+
+        Assert.fail();
+    }
+
+    @Test
+    public void divisionByNegative(){
+        Assert.assertEquals(
+                -2,
+                myCalculator.divide(4, -2)
+        );
+    }
+}
